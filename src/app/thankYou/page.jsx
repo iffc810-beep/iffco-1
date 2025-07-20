22336206c51f66f23a2c26349e8192a50a88d1c4
iffc810-beep <iffc810@gmail.com>
2025-07-20 06:01:21 +0530

    aaaaa

--- src/app/thankYou/page.jsx
+++ src/app/thankYou/page.jsx
@@ -1,40 +1,21 @@
-// app/thankyou/page.jsx
-'use client';
-
-import { useSearchParams } from 'next/navigation';
 import styles from './thankyou.module.css';
-import Image from 'next/image';
 import Link from 'next/link';
+import Image from 'next/image';
 
-const ThankYouPage = () => {
-  const searchParams = useSearchParams();
-  const type = searchParams.get('type');
 
-  let message = "Thank you for your submission!";
-  if (type === 'contact') {
-    message = "Thank you for contacting us. We'll get back to you soon.";
-  } else if (type === 'apply') {
-    message = "Your application has been received. Our team will contact you shortly.";
-  } else if (type === 'subscribe') {
-    message = "You're successfully subscribed to our newsletter!";
-  }
-
+export default function ThankYouPage() {
   return (
-    <section className={styles.thankyouSection}>
+    <div className={styles.thankyouSection}>
       <div className={styles.container}>
-        <Image
-          src="/logo.svg"
-          alt="Logo"
-          width={150}
-          height={150}
-          className={styles.logo}
-        />
+        <Image src="/logo.svg" alt="Logo" className={styles.logo} width={100} height={100} />
         <h1 className={styles.title}>Thank You!</h1>
-        <p className={styles.message}>{message}</p>
-        <Link href="/" className={styles.button}>Go to Homepage</Link>
+        <p className={styles.message}>
+          We appreciate you contacting us. One of our team members will get back to you shortly.
+        </p>
+        <Link href="/" className={styles.button}>
+          Go Back Home
+        </Link>
       </div>
-    </section>
+    </div>
   );
-};
-
-export default ThankYouPage;
+}
